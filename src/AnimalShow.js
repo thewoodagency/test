3cc9a8766c289dfe7adc41579d91b9bbe21f7187
thewoodagency <egnews02@gmail.com>
2023-08-23 23:00:56 -0500

    initial commit

---
+++ src/AnimalShow.js
@@ -0,0 +1,39 @@
+import { useState } from 'react';
+import bird from './svg/bird.svg';
+import cow from './svg/cow.svg';
+import gator from './svg/gator.svg';
+import horse from './svg/horse.svg';
+import cat from './svg/cat.svg';
+import dog from './svg/dog.svg';
+import heart from './svg/heart.svg';
+
+import './AnimalShow.css';
+
+const svgMap = {
+  bird,
+  cow,
+  gator,
+  horse,
+  cat,
+  dog,
+};
+
+function AnimalShow({ type }) {
+  const [clicks, setClicks] = useState(0);
+
+  const handleClicks = () => {
+    setClicks(clicks + 1);
+  };
+
+  return (
+    <div onClick={handleClicks} className="animal">
+      <h3>
+        Show animals: {type} - {clicks}
+      </h3>
+      <img src={svgMap[type]} alt={type} />
+      <img src={heart} alt="heart" style={{ width: 10 + 10 * clicks}}/>
+    </div>
+  );
+}
+
+export default AnimalShow;
--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+import AnimalShow from './AnimalShow';
 import './App.css';
 
+function getAnimal() {
+  const animals = ['bird', 'cat', 'cow', 'dog', 'gator', 'horse'];
+  //console.log(Math.random() * animals.length)
+  return animals[Math.floor(Math.random() * animals.length)];
+}
+
 function App() {
+  const [animals, setAnimals] = useState([]);
+
+  const handleClick = () => {
+    setAnimals([...animals, getAnimal()]);
+  };
+
+  const showAnimals = () => {
+    return animals.map((animal, index) => (
+      <AnimalShow key={index} type={animal} />
+    ));
+  };
+
+  //Array destructuring
+  function makeArray() {
+    return [1, 10, 32, 40];
+  }
+  const [firstItem, secondItem] = makeArray();
+  //console.log(firstItem, secondItem);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <button onClick={handleClick}>Add Animal</button>
+      <div className="row">
+        {showAnimals()}
+      </div>
     </div>
   );
 }
